5399612b0b37b0e56c087d7b1a9c8a329c061f69
hank0982 <hank0982@gmail.com>
2018-08-16 23:24:40 +0800

    delete docs

--- src/components/HeaderUI/index.js
+++ src/components/HeaderUI/index.js
@@ -3,15 +3,17 @@ import {Header, Icon} from 'semantic-ui-react';
 /**
  * @class HeaderUI
  * @extends {Component}
+ * @prop {string} content - the text in header 
+ * @prop {string} icon - the icon id
  * @description This class will generate Header 
  */
 class HeaderUI extends Component {
   render() {
-    const {content, icon} = this.props;
+    const { content, icon } = this.props;
     return (
-        <Header as='h2'>
-            <Icon name={icon} />
-            <Header.Content>{content}</Header.Content>
+        <Header as = 'h2'>
+            <Icon name = { icon } />
+            <Header.Content>{ content }</Header.Content>
         </Header>
     );
   }
