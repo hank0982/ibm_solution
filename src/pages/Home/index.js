3de21420e0dfc953415379eb491606a30a012b55
joannechen1223 <b04701232@ntu.edu.tw>
2018-08-20 20:16:52 +0800

    add flip-page and filter

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import HeaderUI from '../../components/HeaderUI';
 import ProductGrids from '../../components/ProductGrids'
-import { Container } from 'semantic-ui-react';
+import { Container, Button } from 'semantic-ui-react';
 import Chance from 'chance';
 const chance = new Chance();
 /**
@@ -22,7 +22,7 @@ const carDataGenerator = (dataNum) => {
       header: `${randomCarName}-${chance.character({ alpha: true, casing: 'upper' })}${Math.floor(Math.random() * 1000)}`,
       metaData: chance.date({string: true}),
       description: chance.sentence({ words: 3 }),
-      salesPerson: chance.first() + chance.last()
+      salesPerson: chance.first() + chance.last(),
     })
   }
   return data;
@@ -34,14 +34,99 @@ const carDataGenerator = (dataNum) => {
  * This class is responsible for main page
  */
 class Home extends Component {
+  constructor(){
+    super();
+    this.state = {
+      carData: [],
+      //carShow: [],
+      page: 1,
+    };
+    this.handleCarShowChange = this.handleCarShowChange.bind(this);
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+
+  
+
+  
+
+  handlePageChange(e, dir){
+    console.log('in handlePageChange');
+    if (dir === 'right') {
+      this.setState({
+        page: this.state.page + 1,
+      });
+    } else if (dir === 'left') {
+      if(this.state.page !== 1){
+        this.setState({
+          page: this.state.page - 1,
+        });
+      }
+    }
+    
+  }
+
+  handleCarShowChange(searchWord) {
+    let newCarShow = [];
+    let newCarFilter = [];
+    const carData = this.state.carData;
+    if (searchWord === undefined || searchWord === ' ') {
+      newCarFilter = carData;
+    }
+    else{
+      for ( let i = 0; i < carData.length; i += 1) {
+        if (carData[i].header.toLowerCase().indexOf(searchWord.toLowerCase())>-1) {
+          newCarFilter.push(carData[i]);
+        }
+      }
+    }
+    const gridInEachLine = 4;
+    const lineInEachPage = 4;
+    const page = this.state.page;
+    const data = newCarFilter;
+    const carsInEachPage = gridInEachLine * lineInEachPage;
+    for ( let i = 0; i < carsInEachPage; i += 1) {
+      if(data[(page - 1) * carsInEachPage + i] === undefined) break;
+      newCarShow.push(data[(page - 1) * 16 + i]);
+      
+    }
+    return newCarShow;
+  }
+
+  componentWillMount() {
+    const initCarData = carDataGenerator(50);
+    this.setState({
+      carData: initCarData,
+    });
+  }
+
   render() {
     const { searchWord } = this.props;
-    const carData = carDataGenerator(29);
+    const carShow = this.handleCarShowChange(searchWord);
     return (
       <div>
       <Container className="Site-content">
         <HeaderUI icon="car" content="Cars"/>
-        <ProductGrids gridInEachLine={4} data={carData} />
+        <ProductGrids
+          page={this.state.page}
+          gridInEachLine={4}
+          lineInEachPage={4}
+          data={carShow}
+        />
+        <div className="Flip-page">
+          <p> --this is page {this.state.page} -- </p>
+          <Button
+            content="Last Page"
+            icon="left arrow"
+            labelPosition="left"
+            onClick={(e) => {this.handlePageChange(e, 'left');}}
+          />
+          <Button
+            content="Next Page"
+            icon="right arrow"
+            labelPosition="right"
+            onClick={(e) => {this.handlePageChange(e, 'right');}}
+          />
+        </div>
       </Container>
       </div>
     );
